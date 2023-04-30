26cd8bad6cbc6a6aadb06fa68ea7bda9545891bc
sebash <sebcampos@criptext.com>
2023-04-30 00:17:04 -0700

    testing video stream

--- rpiserver/rpiserver/asgi.py
+++ rpiserver/rpiserver/asgi.py
@@ -14,7 +14,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
+from CameraSite.routing import websocket_urlpatterns
 
 import django
 
