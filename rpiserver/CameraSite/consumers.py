8bc9b435f2c4abb49a7b7c6f9d0a5ccaa148aeba
sebash <sebcampos@criptext.com>
2023-04-30 00:20:09 -0700

    testing video stream

--- rpiserver/CameraSite/consumers.py
+++ rpiserver/CameraSite/consumers.py
@@ -1,5 +1,5 @@
 from channels.generic.websocket import WebsocketConsumer
-import websockets
+import socket
 import pickle
 import struct
 
@@ -7,28 +7,29 @@ import struct
 class VideoConsumer(WebsocketConsumer):
 
     def connect(self):
-        async with websockets.connect('ws://192.168.7.207:9998') as websocket:
-            self.accept()
-            payload_size = struct.calcsize('Q')
-            data = b""
-            while True:
-                while len(data) < payload_size:
-                    packet = await websocket.recv()
-                    if not packet:
-                        break
-                    data += packet
-                packed_msg_size = data[:payload_size]
-                data = data[payload_size:]
-                msg_size = struct.unpack('Q', packed_msg_size)[0]
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_name = socket.gethostname()
+        port = 9999
+        host_ip = socket.gethostbyname(host_name)
+        client_socket.connect((host_ip, port))
+        data = b""
+        payload_size = struct.calcsize('Q')
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)
+                if not packet:
+                    break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack('Q', packed_msg_size)[0]
 
-                while len(data) < msg_size:
-                    data += await websocket.recv()
-
-                frame_data = data[:msg_size]
-                data = data[msg_size:]
-                frame = pickle.loads(frame_data)
-                await self.send(bytes_data=frame)
-
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            await self.send(bytes_data=frame)
 
     def receive(self, text_data=None, bytes_data=None):
         print(text_data)
