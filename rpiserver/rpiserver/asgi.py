0ae06edb9f66c30b5291c4bb2269626f96815ce9
sebash <sebcampos@criptext.com>
2023-04-30 00:15:32 -0700

    testing video stream

--- rpiserver/rpiserver/asgi.py
+++ rpiserver/rpiserver/asgi.py
@@ -14,9 +14,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from ..CameraSite.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns
 
-# import django
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpiserver.settings")
 
--- rpiserver/rpiserver/urls.py
+++ rpiserver/rpiserver/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('video/', include('CameraSite.urls'))
+   # path('video/', include('CameraSite.urls'))
 ]
